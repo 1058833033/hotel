07cdec75d6ac9428b2bbff0a808b2d5106736634
laris <10058833033@qq.com>
2020-06-18 10:45:10 +0800

    服务调用

---
+++ hotel_eureka/src/main/java/com/qf/hotel/application/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.qf.hotel.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ChenJie
+ * @date 2020-05-14 15:24:50
+ * 功能说明
+ */
+@SpringBootApplication(scanBasePackages = "com.qf.hotel")
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
